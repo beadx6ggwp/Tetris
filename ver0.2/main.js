07a025aaa0f3ccc57fae40dfb05ffdd60b2d7a26
DavidHsu <davidmd9830415@gmail.com>
2017-12-09 03:05:23 +0800

    update

--- ver0.2/main.js
+++ ver0.2/main.js
@@ -13,7 +13,7 @@ var ctx_font = "Consolas",
 var keys = {}, mousePos = { x: 0, y: 0 };
 
 window.onload = function () {
-    ctx = CreateDisplay("myCanvas", maxCol * TS, maxRow * TS);// 12x20,TileSize = 20
+    ctx = CreateDisplay("myCanvas", maxCol * TS + sideMenuWidth, maxRow * TS);// 12x20,TileSize = 20
     width = ctx.canvas.width; height = ctx.canvas.height;
 
 
@@ -27,45 +27,16 @@ window.onload = function () {
 
 }
 
-// ----------------------------------------------------------
-var TS = 40;// TileSize
-var maxCol = 12;
-var maxRow = 20;
-
-var matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0]
-];
-
-var dropCounter = 0;
-var dropInterval = 1;// second
-
-var arena;
-
-var score = 0;
-var player = {
-    pos: { x: 2, y: 2 },
-    matrix: createPiece('T')
-};
-
-var colors = [
-    null,
-    'red',
-    'blue',
-    'violet',
-    'green',
-    'purple',
-    'orange',
-    'pink'
-];
-
 //----------------------------------------------------------
 
 function main() {
     console.log("Start");
 
     arena = createMatrix(12, 20);
+
+    for (let i = 0; i < 3; i++) {
+        queue.push(getRandomBlack());
+    }
 
     window.requestAnimationFrame(mainLoop);
     //mainLoop();
@@ -102,23 +73,89 @@ function update(dt) {
     dropCounter += dt;
     if (dropCounter > dropInterval) {
         playerDrop();
+        dropCounter = Math.max(dropCounter - dropInterval, 0);
+    }
+
+    if (isShake) {
+        let i = shakeIndex % next.length;
+        canvasPos.x = next[i].x;
+        canvasPos.y = next[i].y;
+
+        shakeIndex++;
+        shakeCounter += dt;
+        if (shakeCounter >= shakeTime) {
+            isShake = 0;
+            shakeCounter = 0;
+            shakeIndex = 0;
+            ctx.resetTransform();
+        }
     }
 }
 
 function draw(ctx) {
-    drawBack();
+    if (isShake) {
+        ctx.translate(canvasPos.x, canvasPos.y);
+    }
 
     drawMatrix(arena, { x: 0, y: 0 });// 背景物體
     drawMatrix(player.matrix, player.pos);// 當前物體
+
+    drawMenu();
     drawString(ctx, score + "",
         10, 10,
         "rgba(255,255,255)", 36, "consolas",
         0, 0, 0);
 }
 
+// ----------------------------------------------------------
+var sideMenuWidth = 100;
+
+var shakeTime = 0.2;
+var isShake = 0;
+var shakeCounter = 0;
+var canvasPos = { x: 0, y: 0 };
+var shakeIndex = 0;
+var shakePower = 3;
+var next = [
+    { x: shakePower, y: 0 },
+    { x: 0, y: shakePower },
+    { x: -shakePower, y: 0 },
+    { x: 0, y: -shakePower }
+];
+
+var TS = 40;// TileSize
+var maxCol = 12;
+var maxRow = 20;
+
+
+var dropCounter = 0;
+var dropInterval = 1;// second
+
+var arena;
+
+var score = 0;
+var player = {
+    pos: { x: 2, y: 2 },
+    matrix: getRandomBlack()
+};
+
+var queue = [];
+
+var colors = [
+    null,
+    'red',
+    'blue',
+    'violet',
+    'green',
+    'purple',
+    'orange',
+    'pink'
+];
+
 //---------------------------------------------
 
-function drawBack() {
+function drawMenu() {
+    // 畫出預測位置
     var clone = {
         matrix: player.matrix,
         pos: Object.assign({}, player.pos)
@@ -130,6 +167,39 @@ function drawBack() {
     ctx.globalAlpha = 0.3;
     drawMatrix(clone.matrix, clone.pos);
     ctx.globalAlpha = 1;
+
+    ctx.fillStyle = "#222";
+    ctx.fillRect(width - sideMenuWidth, 0, sideMenuWidth, height);
+
+    // 方塊佇列
+    let size = 30;
+    let offsetY = 50;
+    let offsetX = width - size * queue.length;
+    queue.forEach((arr, index) => {
+        arr.forEach((row, y) => {
+            row.forEach((val, x) => {
+                if (val != 0) {
+                    ctx.fillStyle = colors[val];
+                    ctx.fillRect(
+                        (x) * size + offsetX,
+                        (y) * size + offsetY,
+                        size, size);
+                }
+            });
+        });
+
+        offsetY += size * 5;
+    });
+
+    drawString(ctx, "Next",
+        offsetX, 0,
+        "rgba(255,255,255)", 32, "consolas",
+        0, 0, 0);
+    //↑→↓←
+    drawString(ctx, "Move:\nLeft,Right\n\nRotate:\nUp\n\nSoft Drop:\nDown\n\nHard Drop:\nSpace",
+        offsetX, height * 3 / 4,
+        "rgba(255,255,255)", 14, "consolas",
+        0, 0, 0);
 }
 
 function drawMatrix(matrix, offset, color) {
@@ -138,6 +208,11 @@ function drawMatrix(matrix, offset, color) {
             if (val != 0) {
                 ctx.fillStyle = color ? color : colors[val];
                 ctx.fillRect(
+                    (x + offset.x) * TS,
+                    (y + offset.y) * TS,
+                    TS, TS);
+                ctx.strokeStyle = color ? color : colors[val];
+                ctx.strokeRect(
                     (x + offset.x) * TS,
                     (y + offset.y) * TS,
                     TS, TS);
@@ -162,6 +237,8 @@ function arenaSweep() {
             arena.unshift(row);
             y++;
             score++;
+
+            isShake = 1;
         }
     }
 }
@@ -247,7 +324,6 @@ function playerDrop() {
         arenaSweep();
         result = 0;
     }
-    dropCounter = Math.max(dropCounter - dropInterval, 0);
 
     return result;
 }
@@ -260,8 +336,8 @@ function playerMove(dir) {
 }
 
 function playerReset() {
-    let pieces = 'ILJOTSZ';
-    player.matrix = createPiece(pieces[Math.floor(pieces.length * Math.random()) | 0]);
+    queue.push(getRandomBlack());
+    player.matrix = queue.shift();
     player.pos.y = 0;
     player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
 
@@ -272,6 +348,10 @@ function playerReset() {
     }
 }
 
+function getRandomBlack() {
+    let pieces = 'ILJOTSZ';
+    return createPiece(pieces[Math.floor(pieces.length * Math.random()) | 0]);
+}
 function createPiece(type) {
     if (type === 'T') {
         return [
